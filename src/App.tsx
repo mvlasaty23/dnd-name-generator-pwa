3118fc35a0d6f2eb1283b830864672ef9cc73e01
Michael Vlasaty <mvlasaty23@gmail.com>
2021-02-07 11:20:41 +0100

    implement app skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,64 @@
+import { Button, createStyles, Grid, makeStyles, TextField, Theme, Typography } from '@material-ui/core';
+import RefreshIcon from '@material-ui/icons/Refresh';
+import 'fontsource-roboto';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const prefix = ['asd', 'das', 'sad', 'def', 'fed', 'aed', 'dea', 'oid', 'dio', 'odi', 'uid'];
+
+const suffix = ['asd', 'das', 'sad', 'def', 'fed', 'aed', 'dea', 'oid', 'dio', 'odi', 'uid'];
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    generateBtn: {
+      textAlign: 'left',
+    },
+  }),
+);
+
 function App() {
+  function generateRandomName(...syllables: string[][]): string {
+    function randomizeIndex(upper: number): number {
+      return Math.floor(Math.random() * upper + 1);
+    }
+    function notSame(idx: number | undefined) {
+      return (targetIdx: number): number => (idx === undefined || targetIdx != idx ? targetIdx : targetIdx - 1);
+    }
+    function firstUpper(value: string): string {
+      return value[0].toUpperCase() + value.slice(1);
+    }
+    const indezes = syllables.map((syllable) => randomizeIndex(syllable.length - 1));
+    return syllables.map((syllable, idx) => syllable[notSame(indezes[idx - 1])(indezes[idx])]).join(' ');
+  }
+
+  const classes = useStyles();
+  const [name, setName] = React.useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <Typography variant="h3" component="h2" gutterBottom>
+        Name Generator
+      </Typography>
+      <form noValidate autoComplete="off">
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TextField id="input-name" label="Randomized name" value={name} />
+          </Grid>
+          <Grid item xs={6}></Grid>
+          <Grid item xs={6} className={classes.generateBtn}>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => setName(generateRandomName(prefix, suffix))}
+              startIcon={<RefreshIcon />}
+              disableElevation
+            >
+              Generate
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </main>
   );
 }
 
