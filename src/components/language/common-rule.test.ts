877dfaedff0100e088c112cbca16ae121f37849f
Michael Vlasaty <mvlasaty23@gmail.com>
2021-03-07 18:31:56 +0100

    add more common language rules

--- src/components/language/common-rule.test.ts
+++ src/components/language/common-rule.test.ts
@@ -1,5 +1,14 @@
 import { LanguageRule } from '../model';
-import { distinct, notSameSubsequentComposite, notSameSubsequentVocal } from './common-rule';
+import {
+  distinct,
+  notAfter,
+  notMoreThan,
+  notMoreThanExcept,
+  notSameStarting,
+  notSameSubsequentComposite,
+  notSameSubsequentVocal,
+  uniqueSubstring,
+} from './common-rule';
 
 describe('Common Language Rule', () => {
   describe('Not Same After', () => {
@@ -18,9 +27,79 @@ describe('Common Language Rule', () => {
       { word: ['ast'], nextSyllable: 'ste' },
     ].forEach((testCase) =>
       it(`should not allow syllable[${testCase.nextSyllable}] to be added to word[${testCase.word}]`, () => {
-        expect(evaluateRules(rules, testCase.word, testCase.nextSyllable)).not.toBeUndefined();
+        expect(evaluateRules(rules, testCase.word, testCase.nextSyllable)).not.toBe(-1);
       }),
     );
+  });
+
+  describe('Not after', () => {
+    const rules = [notAfter('d', 'd'), notAfter('t', 'd'), notAfter('th', 'd')];
+    it('should not allow next syllable starting with d after a syllable ending with d, t or th', () => {
+      // Given
+      const word1 = ['ad'];
+      const word2 = ['at'];
+      const word3 = ['ath'];
+      const word4 = ['al'];
+      const nextSyllable = 'da';
+
+      // When + Then
+      expect(evaluateRules(rules, word1, nextSyllable)).not.toBe(-1);
+      expect(evaluateRules(rules, word2, nextSyllable)).not.toBe(-1);
+      expect(evaluateRules(rules, word3, nextSyllable)).not.toBe(-1);
+      expect(evaluateRules(rules, word4, nextSyllable)).toBe(-1);
+    });
+  });
+
+  describe('Unique Substring', () => {
+    const rules = [uniqueSubstring('y')];
+    it('should not allow syllables containing the unique substring', () => {
+      // Given
+      const word1 = ['asy'];
+      const word2 = ['as'];
+      const nextSyllable = 'by';
+      // When + Then
+      expect(evaluateRules(rules, word1, nextSyllable)).not.toBe(-1);
+      expect(evaluateRules(rules, word2, nextSyllable)).toBe(-1);
+    });
+  });
+
+  describe('Not same startin', () => {
+    const rules = [notSameStarting('c')];
+    it('should not allow subsequent syllables with the same start', () => {
+      // Given
+      const word1 = ['cy'];
+      const word2 = ['ad', 'sa'];
+      const nextSyllable = 'ca';
+      // When + Then
+      expect(evaluateRules(rules, word1, nextSyllable)).not.toBe(-1);
+      expect(evaluateRules(rules, word2, nextSyllable)).toBe(-1);
+    });
+  });
+
+  describe('Not more than', () => {
+    const rules = [notMoreThan('i', 3)];
+    it('should not allow more occurences of the given substring', () => {
+      // Given
+      const word1 = ['ci', 'saii'];
+      const word2 = ['adi', 'sai'];
+      const nextSyllable = 'ci';
+      // When + Then
+      expect(evaluateRules(rules, word1, nextSyllable)).not.toBe(-1);
+      expect(evaluateRules(rules, word2, nextSyllable)).toBe(-1);
+    });
+  });
+
+  describe('Not more than except', () => {
+    const rules = [notMoreThanExcept('a', 3, 'ae')];
+    it('should not allow more occurences of the given substring except for defined combination', () => {
+      // Given
+      const word1 = ['ca', 'sama'];
+      const word2 = ['cae', 'sai'];
+      const nextSyllable = 'ma';
+      // When + Then
+      expect(evaluateRules(rules, word1, nextSyllable)).not.toBe(-1);
+      expect(evaluateRules(rules, word2, nextSyllable)).toBe(-1);
+    });
   });
 
   describe('Distinct', () => {
@@ -31,15 +110,15 @@ describe('Common Language Rule', () => {
       { word: ['asd', 'daf'], nextSyllable: 'asd' },
     ].forEach(({ word, nextSyllable }) =>
       it(`should not allow duplicat[${nextSyllable}] in word[${word}]`, () => {
-        expect(evaluateRules(rules, word, nextSyllable)).not.toBeUndefined();
+        expect(evaluateRules(rules, word, nextSyllable)).not.toBe(-1);
       }),
     );
     it('should allow unique syllables', () => {
-      expect(evaluateRules(rules, ['asd', 'daf'], 'blah')).toBeUndefined();
+      expect(evaluateRules(rules, ['asd', 'daf'], 'blah')).toBe(-1);
     });
   });
 });
 
 function evaluateRules(rules: LanguageRule[], word: string[], syllable: string) {
-  return rules.find((rule) => !rule(syllable, word));
+  return rules.findIndex((rule) => !rule(syllable, word));
 }
--- src/components/language/common-rule.ts
+++ src/components/language/common-rule.ts
@@ -1,13 +1,40 @@
 export type Vocals = 'a' | 'e' | 'i' | 'o' | 'u';
 export type CompositeSyllable = 'ck' | 'ch' | 'sch' | 'tz' | 'th' | 'st';
 
-export function notSameAfter<T extends string = Vocals | CompositeSyllable>(vocal: T) {
-  return (syllable: string, word: string[]) => !(syllable.startsWith(vocal) && word[word.length - 1].endsWith(vocal));
-}
 function distinctSyllables() {
   return (syllable: string, word: string[]) => word.find((el) => el === syllable) === undefined;
 }
-
+export function notAfter(ending: string, nextSub: string) {
+  return (syllable: string, word: string[]) =>
+    !(syllable.startsWith(nextSub) && word[word.length - 1].endsWith(ending));
+}
+export function notSameAfter<T extends string = Vocals | CompositeSyllable>(vocal: T) {
+  return notAfter(vocal, vocal);
+}
+export function uniqueSubstring(substring: string) {
+  return (syllable: string, word: string[]) =>
+    !(syllable.includes(substring) && word.findIndex((w) => w.includes(substring)) > -1);
+}
+export function notSameStarting(substring: string) {
+  return (syllable: string, word: string[]) =>
+    !(syllable.startsWith(substring) && word[word.length - 1].startsWith(substring));
+}
+export function notMoreThan(substring: string, count: number) {
+  return (syllable: string, word: string[]) =>
+    !(
+      syllable.includes(substring) &&
+      word.map((w) => w.split(substring).length - 1).reduce((prev, next) => prev + next, 0) >= count
+    );
+}
+export function notMoreThanExcept(substring: string, count: number, except: string) {
+  return (syllable: string, word: string[]) =>
+    Math.abs(syllable.split(substring).length - 1 - (syllable.split(except).length - 1)) +
+      Math.abs(
+        word.map((w) => w.split(substring).length - 1).reduce((prev, next) => prev + next, 0) -
+          word.map((w) => w.split(except).length - 1).reduce((prev, next) => prev + next, 0),
+      ) <=
+    count;
+}
 export const notSameSubsequentVocal = [
   notSameAfter('a'),
   notSameAfter('e'),
